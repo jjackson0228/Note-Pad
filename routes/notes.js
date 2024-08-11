5ddc45621dbf185fb62e47f4e03791b44605d7bb
James Jackson <jjackson0228@gmail.com>
2024-08-11 18:18:55 -0400

    almost finished with notes.js adding the loops to check and parse data

--- routes/notes.js
+++ routes/notes.js
@@ -5,3 +5,50 @@ const { v4: uuidv4 } = require("uuid");
 const util = require("util");
 
 const dbPath = "./db/db.json";
+
+//get api route for notes
+notes.get("/", (req, res) => {
+  fs.readFile(dbPath, "utf8", (err, data) => {
+    //if, else, try ,catch, loop that checks for errors and logs to console if error is present. If no error tries to parse json data
+    if (err) {
+      console.error(err);
+    } else {
+      try {
+        res.json(JSON.parse(data));
+      } catch (e) {
+        res.json("");
+      }
+    }
+  });
+});
+
+//post api routes fro notes
+notes.post("/", (req, res) => {
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      id: uuidv4(),
+    };
+    fs.readFile(dbPath, "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        let notesArray;
+        try {
+          notesArray = JSON.parse(data);
+        } catch (e) {
+          notesArray = [];
+        }
+        notesArray.push(newNote);
+        fs.writeFile("./db/db.json", JSON.stringify(notesArray), (err) =>
+          err ? console.error(err) : res.json(newNote)
+        );
+      }
+    });
+  } else {
+    res.error("An error occured while attempting to add tip");
+  }
+});
